8b20c429ea83004dc6db527b78e2987c9ada6ba7
Aden Lofthouse <aden.lofthouse@gmail.com>
2023-04-28 13:51:25 +1000

    added scraper to github

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [products, setProducts] = useState([]);
 
   async function getProducts() {
-    let { data: product } = await supabase.from("4wdsupacentre").select("*");
+    let { data: product } = await supabase.from("product").select("*");
     setProducts(product);
   }
 
@@ -24,18 +24,24 @@ export default function Home() {
       className="
          m-4  h-3/4 rounded-3xl"
     >
-      <h1 className="text-5xl m-10 text-center">
-        PRICE TRACKER FOR 4WD SUPACENTRE
-      </h1>
+      <div className="m-10 text-center">
+        <h1 className="text-5xl m-3">PRICE TRACKER FOR 4WD SUPACENTRE</h1>
+        <h2 className="m-3 border-black border-2">
+          This a basic tool to help track the price history of 4wd Supacentre.
+          It is a work in progress and will evolve over time. If you have any
+          issues or ideas for new features please contact me omni4x4@gmail.com
+        </h2>
+        <h2 className="m-3">
+          To search the table, click the "filters" button below and choose which
+          column you would like to filter by
+        </h2>
+      </div>
       <Box
         height="80vh"
         sx={{
           ...Gridsettings,
         }}
       >
-        {/* <div className="p-5">
-          <SearchBar />
-        </div> */}
         <DataGrid
           checkboxSelection
           rows={products}
---
+++ scraper/Script/4wdsupacentre.py
@@ -0,0 +1,120 @@
+from urllib import response
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from scrapy import Selector
+import csv
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+# Setting up the driver
+chromeOptions = Options() # Setting up the chrome options
+chromeOptions.add_experimental_option("excludeSwitches", ["enable-logging"]) # Disabling the logging
+chromeOptions.add_argument("--headless") # Running the driver in headless mode 
+chromePath = "chromedriver.exe" # Path to the chrome driver
+driver = webdriver.Chrome(executable_path = chromePath, options = chromeOptions) # Setting up the driver
+driver.maximize_window() # Maximizing the window
+
+
+
+
+driver.get("https://www.4wdsupacentre.com.au/products.html?page=1") # Opening the website
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="itemCountContainer-3Wi"]')))
+response = Selector(text=driver.page_source) # Converting the page source to a response object
+
+products = response.xpath('//div[@class="itemCountContainer-3Wi"]/text()').extract_first()
+products = products.strip()
+products = products.split(" ")
+products = products[0]
+products = int(products)
+total_pages = products / 12 
+
+if total_pages > int(total_pages):
+   total_pages = int(total_pages) + 2
+else:
+   total_pages = int(total_pages) + 1
+# print("Total Pages: ", total_pages)
+
+
+
+
+# Opening the CSV File to write data in it
+with open('4wdsupacentre.csv', 'w', newline='', encoding="utf-8-sig") as csvfile: 
+   fieldnames = ["Product Link", "Product Name", "Product Price", "Shipping Price", "SKU"] # adjust fieldnames
+   writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+   writer.writeheader()
+   
+   for i in range(1, int(total_pages)): # Looping through all the pages
+      link = "https://www.4wdsupacentre.com.au/products.html?page=" + str(i) # Creating the link
+      driver.get(link)
+      WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="root-2fi null"]')))
+      response = Selector(text=driver.page_source) # Converting the page source to a response object
+
+
+      listings = response.xpath('//div[@class="root-2fi null"]') # Scraping all the listings on the page
+      for listing in listings:
+         product_link = listing.xpath('.//div[@class="name-1QF"]/a/@href').extract_first()
+         
+
+         product_name = listing.xpath('.//div[@class="name-1QF"]/a/text()').extract_first() # Scraping product name
+         if product_name:
+            product_name = product_name.strip()
+
+           
+         product_price = listing.xpath('.//div[@class="priceLine-1H-"]/div/span/text()').extract() # Scraping product price
+         if product_price:
+            product_price = "".join(product_price)
+            product_price = product_price.strip()
+
+
+         shipping_price = listing.xpath('.//span[@class="freightvalue-2VW"]/span/text()').extract() # Scraping shipping price
+         if shipping_price:
+            shipping_price = "".join(shipping_price)
+            shipping_price = shipping_price.strip()
+            shipping_price = shipping_price.replace("+ ", "")
+         else:
+            shipping_price = ""
+
+
+
+
+         product_link = "https://www.4wdsupacentre.com.au" + product_link # Creating the product link
+         driver.get(product_link)
+
+         try:
+            WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, '//meta[@itemprop="sku"]')))
+     
+            response = Selector(text=driver.page_source) # Converting the page source to a response object
+
+            sku = response.xpath('//meta[@itemprop="sku"]/@content').extract_first() # Scrapping SKU 
+            if sku:
+               sku = sku.strip()
+
+            # # Printing data on console 
+            # print("-------------------------------------------------------------------")
+            # print("Page Number: ", i)
+            # print("Product Link: ", product_link)
+            # print("Product Name: ", product_name)
+            # print("Product Price: ", product_price)
+            # print("Shipping Price: ", shipping_price)
+            # print("SKU: ", sku)
+            # print("-------------------------------------------------------------------")
+
+            # Writing data to CSV File
+            writer.writerow({
+               "Product Link": product_link,
+               "Product Name": product_name,
+               "Product Price": product_price,
+               "Shipping Price": shipping_price,
+               "SKU": sku
+            })
+         except:
+            pass
+      print("Pages Scraped: ", i)
+
+print("The Scraping has finished successfully!")
+driver.close() # Closing the driver
+driver.quit() # Quitting the driver
